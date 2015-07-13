2aafe73652929abb62533aa3aafb497d7df0d046
TND23 <tommyn@isomedia.com>
2015-07-13 09:51:02 -0700

    Revision 1

--- markov_gen.rb
+++ markov_gen.rb
@@ -13,6 +13,8 @@ class MarkovText
   end
 
   # store the length of the first text and combine that with the length fo all the other texts
+  # implemented in this manner in case the first text is a string and the others are entered as arrays: it would make more sense
+  # to have the entire input just be an array of arbitrary length but w/e
   def population_size
     first_text_length = [@initial_text.split(" ").length]
     l2 = []
@@ -21,23 +23,9 @@ class MarkovText
   end
 
   def split_initial
-    @initial_text.split(" ")
+    @initial_text.split(" ").split(".").split(",")
   end
 
-  def word_quantities
-    h = {}
-    s_t = @lump_texts.join(" ")
-    j_t = s_t.split(" ").split(".").split(",")
-    j_t.each do |el|
-      if h[el]
-        h[el] += 1
-      else
-        h[el] = 1
-      end
-    end
-    p h
-
-  end
   # Read each element, where an element is defined as a sequence of characters seperated by a space or quotation mark
   # before the first alphabetic or numeric character, and a space or punctuation after the last alphabetic or numeric character
 
@@ -49,7 +37,6 @@ class MarkovText
 
     hash = {}
     pop_size = population_size-2
-    p1 = Time.now
     for i in 0...pop_size
       # notice that by creating this double that we can use tmp[0] in the first if clause, and tmp[1] inside that if clause, and only have iterated once
       tmp = [@lump_texts[i], @lump_texts[i+1]]
@@ -59,27 +46,28 @@ class MarkovText
         hash[tmp[0]] = [tmp[1]]
       end
     end
-    p2 = Time.now
-    puts p2 - p1
+
     hash
   end
+
 
   # notice that we iterate three times in this implementation in the first if clause alone
+  # first implementation: note the suck
 
-  def create_text_hashes
+  # def create_text_hashes
 
-    hash = {}
-    pop_size = population_size-1
+  #   hash = {}
+  #   pop_size = population_size-1
 
-    for i in 0...pop_size
-      if hash[@lump_texts[i]]
-        hash[@lump_texts[i]] << @lump_texts[i+1]
-      else 
-        hash[@lump_texts[i]] = [@lump_texts[i+1]]
-      end
-    end
-    hash
-  end
+  #   for i in 0...pop_size
+  #     if hash[@lump_texts[i]]
+  #       hash[@lump_texts[i]] << @lump_texts[i+1]
+  #     else 
+  #       hash[@lump_texts[i]] = [@lump_texts[i+1]]
+  #     end
+  #   end
+  #   hash
+  # end
 
 
   def convert_text(text)
@@ -91,16 +79,18 @@ class MarkovText
     f
   end
 
+  # get rid of the nasty newlines and tabs
   def open_and_parse_text_file(filename)
     parsed_text = File.open(filename){|file| file.read}
     parsed_text.gsub(/\n/, " ").gsub("/\t/", " ")
   end
 
+  # lump all the texts together into one big old array
   def create_lump_texts
     @lump_texts = split_initial
     v = []
     @other_texts.each do |el|
-      v.push el.split(" ")
+      v.push el.split(" ").split(".").split(",")
     end
     @lump_texts +=  v.flatten!
     # @other_texts.map{|text| @lump_texts << text.split(" ")}
@@ -120,7 +110,7 @@ class MarkovText
 end
 
 
-v = MarkovText.new("./markov_sample_texts/forrest_gump.txt", "./markov_sample_texts/ace_ventura.txt")
+v = MarkovText.new("/../markov_sample_texts/forrest_gump.txt", "/../markov_sample_texts/ace_ventura.txt")
 
 v.create_lump_texts
 
